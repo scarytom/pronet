f85c8df535984a2ce70699e709a5d46f3d5a14a7
Tom Denley <t.denley@cantab.net>
2012-06-14 14:54:54 +0100

    a programmer

---
+++ src/main/java/com/scarytom/pronet/Programmer.java
@@ -0,0 +1,17 @@
+package com.scarytom.pronet;
+
+import java.util.Set;
+
+public class Programmer {
+	public Set<Programmer> recommendations(){
+		return null;
+	}
+	
+	public String name(){
+		return null;
+	}
+	
+	public Set<String> skills(){
+		return null;
+	}
+}
