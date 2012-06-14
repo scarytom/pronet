85893975eb983dd6ffcd6c94bece8c7e54de118e
davie moston <daviemoston@gmail.com>
2012-06-14 15:07:12 +0100

    fields

--- src/main/java/com/scarytom/pronet/Programmer.java
+++ src/main/java/com/scarytom/pronet/Programmer.java
@@ -1,14 +1,23 @@
 package com.scarytom.pronet;
 
+import java.util.List;
 import java.util.Set;
 
 public class Programmer {
+	private final String name;
+	private final Set<Programmer> recommendations;
+
+	public Programmer(String name, Set<Programmer> recommendations) {
+		this.name = name;
+		this.recommendations = recommendations;
+	}
+
 	public Set<Programmer> recommendations(){
-		return null;
+		return recommendations;
 	}
 	
 	public String name(){
-		return null;
+		return name;
 	}
 	
 	public Set<String> skills(){
