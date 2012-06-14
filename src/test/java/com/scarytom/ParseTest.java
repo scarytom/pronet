bd345baecca3d4f277450f36bbc9126c92ff652e
mike <mike@Michael-Hills-MacBook-Air.local>
2012-06-14 16:06:57 +0100

    Mike Ignasi

--- src/test/java/com/scarytom/ParseTest.java
+++ src/test/java/com/scarytom/ParseTest.java
@@ -1,5 +1,8 @@
 package com.scarytom;
 
+import com.google.common.collect.Sets;
+import com.scarytom.pronet.Programmer;
+import nu.xom.*;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
@@ -9,9 +12,20 @@ import java.io.IOException;
 public class ParseTest {
 
     @Test
-    public void parse() throws IOException {
+    public void parse() throws IOException, ParsingException {
         File file = new File("src/main/resources/ProNet.xml");
-        FileUtils.readFileToString(file);
-        System.out.println(FileUtils.readFileToString(file));
+
+        NetworkBuilder networkBuilder = new NetworkBuilder();
+        Builder parser = new Builder();
+        Document doc = parser.build(file);
+
+        Element rootElement = doc.getRootElement();
+        Elements programmers = rootElement.getChildElements();
+        for (int i = 0; i < programmers.size(); i++) {
+            Programmer programmer = new Programmer(programmers.get(i).getAttribute("name").getValue(), Sets.<String>newHashSet());
+            networkBuilder.withProgrammer(programmer);
+        }
+
+        System.out.println(networkBuilder.build().printout());
     }
 }
