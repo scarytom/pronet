d048cf7f1fd59eff77012b6e5f2ab7565f02edcc
Tom Denley <t.denley@cantab.net>
2012-06-14 15:48:33 +0100

    undirected graph implementation

--- src/main/java/com/scarytom/UndirectedGraphBuilder.java
+++ src/main/java/com/scarytom/UndirectedGraphBuilder.java
@@ -1,23 +1,31 @@
 package com.scarytom;
 
-import static com.google.common.collect.Sets.newHashSet;
-
-import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.scarytom.pronet.Programmer;
 
 public class UndirectedGraphBuilder {
+	
+	private final Map<Programmer, Set<Programmer>> recommendees = Maps.newHashMap();
+	
 	public UndirectedGraphBuilder(Set<Programmer> programmers) {
+		for (Programmer recommender : programmers) {
+			Set<Programmer> recommendations = recommender.recommendations();
+			for (Programmer recommendee : recommendations) {
+				Set<Programmer> current = recommendees.get(recommendee);
+				if (current == null) {
+					current = Sets.newHashSet();
+					recommendees.put(recommendee, current);
+				}
+				current.add(recommender);
+			}
+		}
 	}
 
 	public Set<Programmer> whoRecommends(Programmer programmer) {
-		Set<Programmer> recommendations = newHashSet();
-		Programmer davie = new Programmer("davie", new HashSet<Programmer>());
-		recommendations.add(davie);
-		
-		Programmer tom = new Programmer("tom", recommendations);
-
-		return newHashSet(tom);
+		return recommendees.get(programmer);
 	}
 }
